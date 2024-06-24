46c810db7f1d0daa5e33406fdc677da18526d630
Linda Boughattas <linda.boughattas@enicar.ucar.tn>
2024-06-24 11:45:44 +0200

    created an add card button and replaced startgame with render function (shows only the new sum)

--- index.js
+++ index.js
@@ -5,15 +5,31 @@ let hasBlackJack = false
 let isAlive = true
 let message = ""
 
-function startGame(){
-if (sum <= 20) {
-    message = "Do you want to draw a new card? 🙂"
-} else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack! 🥳"
-    hasBlackJack = true
-} else {
-    message = "You're out of the game! 😭"
-    isAlive = false
+let messageEl=document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+
+function startGame() {
+    renderGame()
 }
-console.log(message)
+
+function renderGame(){
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card? 🙂"
+    } else if (sum === 21) {
+        message = "You've got Blackjack! 🥳"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game! 😭"
+        isAlive = false
+    }
+    messageEl.textContent=message
+}
+function newCard(){    
+    console.log("Drawing a new card from the deck!")
+    let card = 7
+    sum += card
+    renderGame()
 }
