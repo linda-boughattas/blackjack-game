46fadd2c033d5c35530b2c523f493389a7ab7bca
Linda Boughattas <linda.boughattas@enicar.ucar.tn>
2024-06-24 14:43:05 +0200

    added an object that contains the player's name and chips

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ let messageEl=document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
+let player = {
+    name: "Per",
+    chips: 145
+}
+
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name + ": $" + player.chips
+
 function getRandomCard() {
     let randomNumer= Math.floor( Math.random()*13 ) + 1
     if (randomNumer > 10) {
